45df7052320284f6219f2c2de0cb1ac00edd0e8b
aliagha <aghajani.dev@gmail.com>
2022-06-17 20:50:29 +0430

    stash commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = document.querySelector('#playerscore');
+
+let computerScore = document.querySelector('#computerscore');
 
 function computerPlay() {
     let choice = ['paper', 'rock', 'scissor']
-    return  choice [Math.floor(Math.random()*3)]}
+    return  choice [Math.floor(Math.random()*3)]
+    // console.log()
+}
    
 
- //function to compare every choice 
+
 function playRound(playerSelection,computerSelection) {
   
    //it is possible to put wining condition together but that way can't return what beats like odin wants
@@ -31,48 +34,77 @@ function playRound(playerSelection,computerSelection) {
        return 'You Lose! rock beats scissor';
    }
  }
-function game() {
-    //loop the game for 5 times 
-     for (let i = 0; i < 5; i++) {
-         //catch error and respond for user to write correctly
-       let playerSelection = prompt('please write down "paper"or "rock"or "scissor"', '') ; 
-       if ( playerSelection === '' || playerSelection === null) {
-           alert("please write down one of choices to continue playing");
-            
-       } 
-      //for ending game in middle of play and stoping prompt and clear the scores
-       else if (playerSelection === 'end'){
-            playerScore = 0;
-            computerScore = 0;
-           return "you end the game. for start use 'game()' function ";
-        }
-       //making user input case insensetive ,couldn't put on prompt didn't catch null error 
-       let userInput = playerSelection.toLowerCase();
-       //calling playRound function and puting the value returnd in variable named result
-       let result = playRound(userInput, computerPlay())
-       //show the result on console
-        console.log(result)
-       // condition to add score for each of player and computer
-         if (result == 'You Lose! paper beats rock' || result == 'You Lose! scissor beats paper' || result == 'You Lose! rock beats scissor' ){
-              computerScore++
-            }
-         else if (result == 'You Win! rock beats scissor' || result == 'You Win! paper beats rock' || result == 'You Win! scissor beats paper'){
-              playerScore++
-            }
-    }//END of loop
-    // reveal final score
-    console.log(`computer Score is : ${computerScore}`)
-    console.log(`player Score is : ${playerScore}`)
+
+
+ function game(result) {
+       
+    if (result == 'You Lose! paper beats rock' || result == 'You Lose! scissor beats paper' || result == 'You Lose! rock beats scissor' ){
+        if (playerScore.textContent > 0) 
+         playerScore.textContent--
+
+       }
+    else if (result == 'You Win! rock beats scissor' || result == 'You Win! paper beats rock' || result == 'You Win! scissor beats paper'){
+        if (computerScore.textContent > 0) 
+      
+        computerScore.textContent--
+       }
+}
+
+
+
+function finalReveal(playerScore , computerScore){
+    finalTextBox = document.querySelector('.finalText')
+   console.log(playerScore)
+    if ( computerScore == 0){ 
+        alert("YOU WIN")
+        finalTextBox.textContent= "<<YOU WON THE GAME!!!>>"
+
+       } else if (playerScore == 0 ) {
+        alert ("YOU LOST!")
+        finalTextBox.textContent= "<<YOU LOST THE GAME !>>"
+     }
+    }
+    
+    
+
+
+    
+    document.addEventListener('click', (e) => {
+        let userInput = e.target.id;
+        console.log(userInput)
+        let result = playRound(userInput, computerPlay())
+        //console.log(result)
+        const textBox = document.querySelector('.textBox');
+        textBox.textContent = result;
+        game(result)
+        //console.log(playerScore.textContent)
+        finalReveal(playerScore.textContent,computerScore.textContent)
+     })
+
+
+
+    
+
+     
+    // console.log(`computer Score is : ${computerScore}`)
+    // console.log(`player Score is : ${playerScore}`)
     //condition for wining 
-     if (playerScore > computerScore){ 
-         console.log("<<YOU WON THE GAME!!!>>")
-        } else if (playerScore < computerScore) {
-             console.log("<<YOU LOST THE GAME !>>")
-            } else {
-                console.log("<<THE GAME END UP IN TIE !>>")
-            }    
+    //  if (playerScore > computerScore){ 
+    //      console.log("<<YOU WON THE GAME!!!>>")
+    //     } else if (playerScore < computerScore) {
+    //          console.log("<<YOU LOST THE GAME !>>")
+    //         } else {
+    //             console.log("<<THE GAME END UP IN TIE !>>")
+    //         }    
             
-     //clear the scores
-     playerScore = 0;
-     computerScore = 0;
- }
+    //  //clear the scores
+    //  playerScore = 0;
+    //  computerScore = 0;
+
+
+
+    //  const findId = document.addEventListener('click', (e) => {
+    //    console.log(e.target);
+    //  })
+    
+     
